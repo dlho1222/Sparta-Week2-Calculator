ce9737c087b0ae4088760db0160e17ba01a6663d
ho lee <dlho1222@naver.com>
2023-11-27 15:30:22 +0900

    수정

--- app/src/main/java/com/example/calculator/test.kt
+++ app/src/main/java/com/example/calculator/test.kt
@@ -3,18 +3,24 @@ package com.example.calculator
 fun main() {
     class Calculator {
         var num1 = readln().toInt()
-        var operator : String = readln()
+        var operator: String = readln()
         var num2 = readln().toInt()
+
         fun operation() {
-            var result = when(operator){
-                "+"->   num1 + num2
-                "-"->   num1 - num2
-                "*"->   num1 * num2
-                "/"->   num1 / num2
-                else-> num1 % num2
+            val result = when (operator) {
+                "+" -> num1 + num2
+                "-" -> num1 - num2
+                "*" -> num1 * num2
+                "/" -> num1 / num2
+                "%" -> num1 % num2
+                else -> println("잘못된 연산자 입니다.")
             }
             println("계산결과 $result ")
         }
+
+
     }
     Calculator().operation()
+
+
 }
