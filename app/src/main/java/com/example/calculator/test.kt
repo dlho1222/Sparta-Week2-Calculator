8eb78c39211be0d4782ee0ff1ad9091d310dd211
ho lee <dlho1222@naver.com>
2023-11-27 16:16:18 +0900

    lv2

--- app/src/main/java/com/example/calculator/test.kt
+++ app/src/main/java/com/example/calculator/test.kt
@@ -5,22 +5,32 @@ fun main() {
         var num1 = readln().toInt()
         var operator: String = readln()
         var num2 = readln().toInt()
-
-        fun operation() {
-            val result = when (operator) {
-                "+" -> num1 + num2
-                "-" -> num1 - num2
-                "*" -> num1 * num2
-                "/" -> num1 / num2
-                "%" -> num1 % num2
-                else -> println("잘못된 연산자 입니다.")
+        var result = 0
+        fun operation(): Int {
+            when (operator) {
+                "+" -> result = num1 + num2
+                "-" -> result = num1 - num2
+                "*" -> result = num1 * num2
+                "/" -> result = num1 / num2
+                "%" -> result = num1 % num2
+                else -> print("잘못된 연산 입니다.")
             }
-            println("계산결과 $result ")
+            println("계산 결과 $result")
+            println("추가 계산 하려면 연산자를 입력해 주세요")
+
+            val addOperation = readln()
+            val num3 = readln()!!.toInt()
+            when (addOperation) {
+                "+" -> result += num3
+                "-" -> result -= num3
+                "*" -> result *= num3
+                "/" -> result /= num3
+                "%" -> result %= num3
+                else -> print("잘못된 연산 입니다.")
+            }
+            print("추가 계산결과 : $result")
+            return result
         }
-
-
     }
     Calculator().operation()
-
-
 }
